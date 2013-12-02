9adb09cc1c198323a5fa417db33ec88fa2b78d77
kqb <ktqueenbar@gmail.com>
2013-12-01 23:25:42 -0500

    ss

--- SlidingMenu/src/info/androidhive/slidingmenu/MainActivity.java
+++ SlidingMenu/src/info/androidhive/slidingmenu/MainActivity.java
@@ -6,16 +6,13 @@ import info.androidhive.slidingmenu.model.NavDrawerItem;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.ListFragment;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v4.*;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,7 +20,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity {
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
 	private ActionBarDrawerToggle mDrawerToggle;
@@ -165,46 +162,35 @@ public class MainActivity extends FragmentActivity {
 	private void displayView(int position) {
 		// update the main content by replacing fragments
 		Fragment fragment = null;
-        ListFragment listFragment = null;
 		switch (position) {
-//		case 0:
-//			fragment = new HomeFragment();
-//			break;
-		case 1:
-            listFragment = new MatchesFragment();
+		case 0:
+			fragment = new HomeFragment();
 			break;
-//		case 2:
-//			fragment = new ComparisonFragment();
+//		case 1:
+//            fragment = new MatchesFragment();
 //			break;
-//		case 3:
-//			fragment = new PlayersFragment();
-//			break;
-//		case 4:
-//			fragment = new DotapediaFragment();
-//			break;
-//		case 5:
-//			fragment = new ProfileFragment();
-//			break;
+		case 2:
+			fragment = new ComparisonFragment();
+			break;
+		case 3:
+			fragment = new PlayersFragment();
+			break;
+		case 4:
+			fragment = new DotapediaFragment();
+			break;
+		case 5:
+			fragment = new ProfileFragment();
+			break;
 
 		default:
 			break;
 		}
 
-//		if (fragment != null) {
-//			FragmentManager fragmentManager = getFragmentManager();
-//			fragmentManager.beginTransaction()
-//					.replace(R.id.frame_container, fragment).commit();
-//
-//			// update selected item and title, then close the drawer
-//			mDrawerList.setItemChecked(position, true);
-//			mDrawerList.setSelection(position);
-//			setTitle(navMenuTitles[position]);
-//			mDrawerLayout.closeDrawer(mDrawerList);
-//		}
-        if (listFragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getFragmentManager();
             fragmentManager.beginTransaction()
-                    .replace(R.id.frame_container, listFragment).commit();
+                    .replace(R.id.frame_container, fragment).commit();
 
             // update selected item and title, then close the drawer
             mDrawerList.setItemChecked(position, true);
